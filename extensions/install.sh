4751df4861f8c7d302989cb2b266c3eb9cb0ddcb
Renan Di <renan_di@hotmail.com>
2022-10-04 09:54:00 -0300

    Update install.sh

--- extensions/install.sh
+++ extensions/install.sh
@@ -3,4 +3,7 @@ set -x
 echo "Running $PWD/install.sh"
 injected_dir=$1
 ls -hal $injected_dir
+ls -hal /tmp/src
+cp /tmp/src/ears/ $JBOSS_HOME/deployments
+cp /tmp/src/jboss-modules/ $JBOSS_HOME/modules
 $JBOSS_HOME/bin/jboss-cli.sh --file=$injected_dir/extensions.cli
